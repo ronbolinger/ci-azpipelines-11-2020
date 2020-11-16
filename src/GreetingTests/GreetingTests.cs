770630fed18143887ceac3f4ede98a7d90d0f9fd
ronbolinger <ronbolinger@hotmail.com>
2020-11-16 09:34:41 -0800

    Move GreetingTests to passing green

--- src/GreetingTests/GreetingTests.cs
+++ src/GreetingTests/GreetingTests.cs
@@ -10,11 +10,10 @@ namespace GreetingTests
         public void GreetingTest_MessageContent()
         {
             Greeting.Greeting myGreeting = new Greeting.Greeting();
-            myGreeting.Message = "Not a greeting message";
+            //myGreeting.Message = "Not a greeting message";
 
             string expectedMessage = "Hello World!";
             
-            // Red-phased failing assert
             Assert.AreEqual(expectedMessage, myGreeting.Message);
         }
     }
